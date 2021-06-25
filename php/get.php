707081f548512e17cbfe3e6e6863dcb9d9213256
Manuel Kümpel <46404467+Manuel123cool@users.noreply.github.com>
2021-06-25 17:59:29 +0200

    Update get.php

--- php/get.php
+++ php/get.php
@@ -4,6 +4,9 @@ include "login.php";
 if (isset($_GET["data"]) && validToken()) {
     $path = "/home/manuel/xampp_files/data.txt";
     $data = file_get_contents($path);
+    if($data == false && !empty($data)) {
+        echo "Could not get Data";
+    }
     echo $data;
 } else {
     echo "Not right Token";
