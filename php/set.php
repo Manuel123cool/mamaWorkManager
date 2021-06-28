e006fb47f3133703827f13178f1f2628c5da8ecc
Manuel Kümpel <46404467+Manuel123cool@users.noreply.github.com>
2021-06-28 14:49:08 +0200

    Update set.php

--- php/set.php
+++ php/set.php
@@ -1,7 +1,7 @@
 <?php
 include "login.php";
 
-if (isset($_POST["data"]) && validToken()) {
+if (isset($_POST["data"]) && validLoginData()) {
     $path = "/home/manuel/xampp_files/data.txt";
     if (!file_put_contents($path, $_POST["data"])) {
         echo "Could not set Data";
