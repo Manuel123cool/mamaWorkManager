59cceceee79b0cd1fdf5b713b0a5b5671b83cf6a
Manuel Kümpel <46404467+Manuel123cool@users.noreply.github.com>
2022-07-15 20:28:12 +0200

    Create sendData.php

---
+++ oldServerData/sendData.php
@@ -0,0 +1,9 @@
+<?php
+include "/var/www/manuel-project.de/php/login.php";
+
+if (validLoginData()) {
+	header('Content-Type: text/javascript');
+	echo file_get_contents('/home/manuel/secret_js/data.js');
+} else {
+    echo "Not right Token";
+}
